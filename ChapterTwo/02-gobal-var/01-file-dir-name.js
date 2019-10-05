fef8c2efebfaa071745473d960d6a4a4fe798f6e
zhouhong12345 <15041916476@163.com>
2019-10-05 21:59:44 +0800

    b

--- ChapterTwo/02-gobal-var/01-file-dir-name.js
+++ ChapterTwo/02-gobal-var/01-file-dir-name.js
@@ -2,3 +2,28 @@
 
 console.log('dir name:',__dirname);
 console.log('file name:',__filename);
+
+//operate data file
+
+var file=__dirname + '/data/db.xml';
+console.log('file name:',file);
+
+//window data file
+
+file=__dirname+'\\data\\db.xml';
+console.log('file name in windows:',file);
+
+/*
+switch(os){
+  case 'linux':
+    file=__dirname + '';
+    break;
+  case 'win32':
+    file =__dirname + '';
+    break;
+}*/
+
+const path=require('path');
+
+file=path.join(__dirname,'data','db.xml');
+console.log('path join file name:',file);
